2a0984ebee9038b758bb82b04e5076e7164f5504
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2015-05-08 11:02:29 +0900

    init

---
+++ TwitterClone/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  TwitterClone
+//
+//  Created by Shoya Ishimaru on 2015/05/08.
+//  Copyright (c) 2015年 shoya140. All rights reserved.
+//
+
+#ifndef TwitterClone_Bridging_Header_h
+#define TwitterClone_Bridging_Header_h
+
+#import "Parse.h"
+#import "Bolts.h"
+#import "Key.h"
+
+#endif
